a04c68e6e6ba895c70a3075f3c3a0b99d8c42f70
unknown <caterinailariopaz@alumnos.tuniversformacion.es>
2023-10-18 10:57:38 +0200

    creating inventory

---
+++ scripts/products.js
@@ -0,0 +1,12 @@
+const inventory = [
+    {id: 1, nombre: "Gran Smith Apples", amount:100, price: 40},
+    {id: 2, nombre: "Canary Bananas", amount: 60, price: 30},
+    {id: 3, nombre: "Pineapples", amount: 30, price: 20},
+    {id: 4, nombre: "Oranges", amount: 100, price: 50},
+    {id: 5, nombre: "Peaches", amount: 30, price: 30},
+    {id: 6, nombre: "Pears", amount: 40, price: 15},
+    {id: 7, nombre: "Mandarins", amount: 50, price: 35},
+    {id: 8, nombre: "Kiwis", amount: 60, price: 50},
+    {id: 9, nombre: "Cinnamons", amount: 30, price: 15},
+]
+
