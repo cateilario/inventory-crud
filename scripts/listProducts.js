7076c81a515e668bb8ad87b2556cf5b9052dec1f
Caterina <cateilario@gmail.com>
2023-11-02 23:07:41 +0100

    logic edit product

--- scripts/listProducts.js
+++ scripts/listProducts.js
@@ -15,7 +15,7 @@ export const listProducts = () => {
 
         cell1.innerText = item.name;
         cell2.innerText = item.amount;
-        cell3.innerText = item.price;
+        cell3.innerText = `${item.price}€`;
 
         const editButton = document.createElement("button");
         editButton.innerText = "Editar";
@@ -25,12 +25,32 @@ export const listProducts = () => {
             // Logic to edit specific product
             const modalBox = document.getElementById("modal")
             const closeModal = document.getElementById("btn-close")
-
+            const saveBtn = document.getElementById("btn-save")
+           
             modalBox.style.display = "block";
 
             closeModal.onclick = () =>{
                 modalBox.style.display = "none";
             }
+    
+            saveBtn.addEventListener("click", () => {
+
+                const newName = document.getElementById("name-edit").value
+                const newQuantity = parseInt(document.getElementById("quantity-edit").value)
+                const newPrice = parseFloat(document.getElementById("price-edit").value).toFixed(2)
+    
+                    item.name = newName
+                    item.amount = newQuantity
+                    item.price = newPrice
+
+                    cell1.innerHTML = newName
+                    cell2.innerHTML = newQuantity
+                    cell3.innerHTML = `${newPrice}€`
+
+                    alert('Producto actualizado correctamente')
+                    modalBox.style.display = 'none'
+            })
+
         });
 
         cell4.appendChild(editButton);
---
+++ scripts/searchProduct.js
@@ -0,0 +1,11 @@
+import { inventory } from "./products.js";
+
+
+export const searchProduct = () => { 
+    const searchInput = document.getElementById("search");
+    const searchBtn = document.getElementById("search-btn");
+    const searchOutput = document.getElementById("search-output");
+
+    
+  }
+  
