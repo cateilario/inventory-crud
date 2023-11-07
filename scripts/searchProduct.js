78e5178904954796c662ccacace1d54e9dd33a5c
Cate <caterinailariopaz@alumnos.tuniversformacion.es>
2023-11-07 10:26:29 +0100

    modal2 styles

--- scripts/searchProduct.js
+++ scripts/searchProduct.js
@@ -4,22 +4,20 @@ export const searchProduct = () => {
   const searchInput = document.getElementById("search").value
   const foundedModal = document.getElementById("modal-search")
   const modalBody = document.getElementById("modal-body")
-  const modalClose = document.getElementById("close-modal")
+  const closeBtn = document.getElementById("close-modal")
 
   const productFound = inventory.find(item => item.name.toLowerCase() === searchInput.toLowerCase())
 
   if (productFound){
     foundedModal.style.display = "block"
-    modalBody.innerHTML = `<p>Producto: ${product.name}</p> 
-    <p>Producto: ${product.amount}</p> 
-    <p>Producto: ${product.price}</p> `
 
   } else {
     alert(`Producto no encontrado en el inventario.`)
   }
 
-  modalClose.addEventListener("click", () => {
+  closeBtn.addEventListener("click", () => {
     foundedModal.style.display = "none"
+    searchInput = "";
   })
 
 
